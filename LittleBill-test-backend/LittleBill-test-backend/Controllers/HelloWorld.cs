c77210a41b0a982c9f9a22ff3e65eaf655bd2a5d
PierroD <frozenphenix78@gmail.com>
2021-08-03 01:51:09 +0200

    feat: ajout du fichier "Hello World"

---
+++ LittleBill-test-backend/LittleBill-test-backend/Controllers/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LittleBill_test_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorld : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
